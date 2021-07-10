61056a11ab0f7ed94e8a6c113cc7b7218e55ea4b
Everkers <waelkourich12@gmail.com>
2021-07-10 17:54:01 +0100

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="hero">
+      <div class="hero__background">
+        <img
+          src="https://images.unsplash.com/photo-1551825687-f9de1603ed8b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt=""
+        />
+      </div>
+      <div class="hero__content">
+        <h3 class="hero__content_text">
+          An on demand service that connects you with <br />
+          Bricoleurs through the app
+        </h3>
+        <div class="hero__content_buttons">
+          <button class="hero__content_cta hero__content_cta_white">
+            <img src="../assets/svgs/home.svg" alt="" />
+            Sign up as a consumer
+            <img src="../assets/svgs/arrow_right_white.svg" alt="" />
+          </button>
+          <button class="hero__content_cta hero__content_cta_green">
+            <img src="../assets/svgs/helmet.svg" alt="" />
+            Sign up as a bricoleur
+            <img src="../assets/svgs/arrow_right_black.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </section>
+    <section class="guide">
+      <div class="guide__rows">
+        <h1 class="guide__row_title">How it works</h1>
+        <div class="guide__row guide__row_left">
+          <div>
+            <h2 class="guide__row_title">Request your skhera</h2>
+            <p class="guide__row_desription">Choosing a quality cookware set</p>
+          </div>
+        </div>
+        <div class="guide__row guide__row_right">
+          <div>
+            <h2 class="guide__row_title">Request your skhera</h2>
+            <p class="guide__row_desription">Choosing a quality cookware set</p>
+          </div>
+        </div>
+        <div class="guide__row guide__row_right">
+          <div>
+            <h2 class="guide__row_title">Request your skhera</h2>
+            <p class="guide__row_desription">Choosing a quality cookware set</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mobile-app">
+      <div class="mobile-app__image">
+        <img src="../assets/app.png" alt="" />
+      </div>
+      <div class="mobile-app__content">
+        <h1 class="mobile-app__content_title">
+          Track your deliveries <br />
+          with the Jible App
+        </h1>
+        <div class="mobile-app__content_links">
+          <img
+            src="https://assets.stickpng.com/images/5a902db97f96951c82922874.png"
+            alt=""
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1280px-Google_Play_Store_badge_EN.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<script></script>
